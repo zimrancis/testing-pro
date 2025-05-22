82ecf2e11aee98295a7d94fed4f1b5d603909665
zimrancis <zimran.cis@gmail.com>
2025-05-22 22:39:04 +0600

    Initial commit for this project

---
+++ src/main/java/com/cs/enodes/EnodesApplication.java
@@ -0,0 +1,13 @@
+package com.cs.enodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnodesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cs/enodes/EnodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.enodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
